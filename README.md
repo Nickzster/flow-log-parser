d3cb867cc1686c3fb2501fe6930de440f4efd93f
Nick Zimmermann <nicholas.r.zimmermann@gmail.com>
2024-11-25 18:44:38 -0800

    Initial commit
    - Implement flow-file parser
    - Add README outlining project details
    - Add IANA Protocol Number file

---
+++ README.md
@@ -0,0 +1,29 @@
+# Flow Log Records Parser
+
+This project is a flow log records parser based on [AWS VPC Flow Logs](https://docs.aws.amazon.com/vpc/latest/userguide/flow-log-records.html).
+
+# Setup
+1. Install NodeJS 20.17.0 or newer.
+2. Run the program: `node main.mjs`. This will dump everything into `processed.txt`.
+3. View the output contents via `cat processed.txt`.
+
+For usage help, run `node main.mjs --help`.
+
+# Assumptions
+
+1. This parser can process version 2 default formatted logs. Any other version will be ignored by this parser, and a warning message will be displayed.
+2. The CSV Lookup table only accepts the format of `dstport,protocol,tag`. Any other formats specified in the .csv file will be ignored, and a warning message will be displayed.
+3. Only destination port + protocol counts will be added to the output file.
+4. To get the IANA protocol number mapping, I used [this file](https://github.com/bahamas10/node-protocol-numbers/blob/master/protocol-numbers.js).` 
+
+# Next Steps
+
+Here is what I would like to do with this program next:
+
+1. Build `stdin` and `stdout` functionality so that log messages can be tailed and passed directly in for processing.
+2. Make the program dynamically handle different formats and versions depending on the structure of the log. It would be nice to process each line as it is fed into the program, and update the counts in real time.
+3. Figure out a better way to maintain the IANA protocol numbers. For simplicity sake, I want to keep this program lightweight and free of dependencies as much as possible.
+4. Fix the output processing. Since JavaScript strings are immutable, the string concatenation will need to be improved depending on the input size. 
+5. Get clearer requirements around edge cases and modify the program to handle them.
+
+
---
+++ main.mjs
@@ -0,0 +1,246 @@
+/* 
+flow log parser
+Written in node v20.17.0
+*/
+import path from "path";
+import { promises as fs } from "fs";
+import { protocol_numbers } from "./iana-protocol-numbers.mjs";
+// log file utilities
+
+const AVAILABLE_FIELDS_DEFAULT = [
+  "version",
+  "account-id",
+  "interface-id",
+  "srcaddr",
+  "dstaddr",
+  "srcport",
+  "dstport",
+  "protocol",
+  "packets",
+  "bytes",
+  "start",
+  "end",
+  "action",
+  "log-status",
+];
+
+const handleFields = (key, value) => {
+  if (key === "protocol") {
+    try {
+      const IANAInt = parseInt(value, 10);
+      const protocolName = protocol_numbers[IANAInt].name;
+      return protocolName;
+    } catch (err) {
+      return value;
+    }
+  }
+  return value;
+};
+
+const parseLineFromLog = (line, format) => {
+  const linesArr = line.trim().split(" ");
+
+  if (linesArr.length !== format.length) {
+    console.warn("WARNING: skipping flow log is not in default format");
+    return null;
+  }
+
+  return linesArr.reduce((acc, line, idx) => {
+    const key = format[idx];
+    const value = handleFields(key, line);
+    acc[key] = value;
+    return acc;
+  }, {});
+};
+
+// parseLog parses an input log file and returns an array of log lines.
+// Each line is parsed (parseLine) and builds an object for each field in a single log line.
+// and returns it as an array consisting of these data structures.
+// Any log lines that do not match the default format are skipped.
+const parseLog = (buf) => {
+  const logfile = buf.toString();
+  return logfile
+    .trim()
+    .split("\n")
+    .map((line) => parseLineFromLog(line, AVAILABLE_FIELDS_DEFAULT))
+    .filter((parsed) => parsed !== null);
+};
+
+// lookup table utilities
+
+const serialize = (k, v) => `${k},${v}`.toLowerCase();
+const LOOKUP_DEFAULT_FORMAT = ["dstport", "protocol", "tag"];
+
+const parseCSVLine = (line, format) => {
+  const linesArr = line.trim().split(",");
+
+  if (linesArr.length !== format.length) {
+    console.warn(
+      "WARNING: skipping lookup condition that is not in default format!",
+    );
+    console.log(JSON.stringify(line));
+    return null;
+  }
+
+  if (linesArr[0] === "" && linesArr[1] === "" && linesArr[2] === "") {
+    console.warn("WARNING: skipping lookup condition with empty values!");
+    console.log(JSON.stringify(line));
+    return null;
+  }
+
+  return {
+    key: serialize(linesArr[0], linesArr[1]),
+    value: linesArr[2],
+  };
+};
+
+// buildLookupTable parses a lookup csv file and builds a lookup table in memory.
+// It parses each line (parseCSVLine) and builds a key/value pair mapping.
+// Any malformed lines are skipped.
+const buildLookupTable = (buf) => {
+  const csv = buf.toString();
+  return csv
+    .trim()
+    .split("\n")
+    .reduce((acc, line) => {
+      const parsed = parseCSVLine(line, LOOKUP_DEFAULT_FORMAT);
+      if (parsed !== null) acc[parsed.key] = parsed.value;
+      return acc;
+    }, {});
+};
+
+// command line utilities
+const HELP_TEXT = `
+usage: node main.mjs [OPTIONS]
+  --lookup-file: specify a lookup file. Must be in same directory as program. Defaults to example.csv
+  --output-file: specify an output file. Must be in the same directory as program. Defaults to output.txt
+  --logfile: specify a log file to process. Must be in the same directory as program. Defaults to example.log
+`;
+
+const parseArgs = async () => {
+  const processedArgs = {
+    logfile: "example.log",
+    lookup: "example.csv",
+    output: "processed.txt",
+  };
+
+  for (let i = 0; i < process.argv.length; i++) {
+    const arg = process.argv[i];
+    switch (arg) {
+      case "--logfile": {
+        processedArgs.logfile = process.argv[i + 1];
+        break;
+      }
+
+      case "--lookup-file": {
+        processedArgs.lookup = process.argv[i + 1];
+        break;
+      }
+
+      case "--output-file": {
+        processedArgs.output = process.argv[i + 1];
+        break;
+      }
+
+      case "--help": {
+        console.log(HELP_TEXT);
+        process.exit(0);
+      }
+
+      default:
+        continue;
+    }
+  }
+
+  try {
+    const [logfileIsValid, lookupIsValid, outputIsValid] = await Promise.all([
+      fs.lstat(path.join(process.cwd(), processedArgs.logfile)),
+      fs.lstat(path.join(process.cwd(), processedArgs.lookup)),
+      fs.lstat(path.join(process.cwd(), processedArgs.output)),
+    ]);
+
+    if (!logfileIsValid) {
+      console.error("bad logfile was given", processedArgs.logfile);
+      console.log("logfile specified is invalid. using example.log");
+      return null;
+    }
+
+    if (!lookupIsValid) {
+      console.error("bad lookup was given", processedArgs.lookup);
+      console.log("using example.csv");
+      return null;
+    }
+
+    if (!outputIsValid) {
+      console.error("bad output file was given", processedargs.output);
+      return null;
+    }
+
+    return processedArgs;
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+};
+
+// entry point
+async function main() {
+  const parsedArgs = await parseArgs();
+
+  if (parsedArgs === null) process.exit(1);
+
+  const LOGFILE = parsedArgs.logfile;
+  const LOOKUP = parsedArgs.lookup;
+  const OUTFILE = parsedArgs.output;
+
+  console.log("reading lookup file", path.join(process.cwd(), LOOKUP));
+  const lookupBuffer = await fs.readFile(path.join(process.cwd(), LOOKUP));
+
+  console.log("reading log file", path.join(process.cwd(), LOGFILE));
+  const logfileBuffer = await fs.readFile(path.join(process.cwd(), LOGFILE));
+
+  const tagCounts = new Map([["untagged", 0]]);
+  const portProtocolCounts = new Map();
+
+  const logs = parseLog(logfileBuffer);
+  const table = buildLookupTable(lookupBuffer);
+
+  logs.forEach((log) => {
+    const portProtocolKey = serialize(log.dstport, log.protocol);
+    let tag = table[portProtocolKey];
+
+    // count tags
+    if (tag === undefined) tag = "untagged";
+    if (!tagCounts.get(tag)) tagCounts.set(tag, 0);
+    tagCounts.set(tag, tagCounts.get(tag) + 1);
+
+    // count protocols
+    if (!portProtocolCounts.get(portProtocolKey))
+      portProtocolCounts.set(portProtocolKey, 0);
+
+    portProtocolCounts.set(
+      portProtocolKey,
+      portProtocolCounts.get(portProtocolKey) + 1,
+    );
+  });
+
+  let output = "";
+
+  output += "Tag counts\n";
+  for (let [k, v] of tagCounts) {
+    output += `${k}=${v}\n`;
+  }
+
+  output += "\nDest. Port, Protocol counts\n";
+  for (let [k, v] of portProtocolCounts) {
+    output += `${k}=${v}\n`;
+  }
+
+  const outFile = path.join(process.cwd(), OUTFILE);
+  console.log("writing results to", outFile);
+  await fs.writeFile(outFile, output);
+
+  process.exit(0);
+}
+
+main();
